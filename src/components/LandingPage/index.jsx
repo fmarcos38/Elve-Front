be62efa8b8ebe7bfa313fb600e4fc7250b75454e
fmarcos38 <fmarcos_23@hotmail.com>
2022-12-29 20:09:40 -0300

    dd

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import gif1 from '../../imagenes/joda.gif';
 import gif2 from '../../imagenes/joda2.gif';
+import chino from '../../imagenes/chino.jpg';
 import './landing.css'
 
 
@@ -8,12 +9,16 @@ function LandingPage() {
 
   return (
     <div className='contenedorGral'>
+        
         <h1 className='titulo'>Chupenme bien la japi ..Putitas!!!</h1>
-        <img src={gif1}/>
+        <img src={gif1} className="gifs"/>
         <h1 className='titulo'>Esta se la dedico al Vikingo Sexy!!!</h1>
-        <img src={gif2}/>
+        <img src={gif2} className="gifs"/>
+    
         <h1 className='titulo'>Feliz año...Putitas!!!</h1>
-
+        <img src={chino} className="chino"/>
+        
+        
     </div>
   )
 }
